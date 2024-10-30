78ff5b3796f973134e35fb3ca5445267c9d5b26e
Matheus Pinter <matheuspinter08@gmail.com>
2024-10-30 00:10:32 -0300

    feat: Added game-creation and how-it-works sections, responsivity of game-creation almost finished

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 const openModal = (header, modal, body) => {
- 
+
     modal.style.display = 'block'
     modal.classList.add("active")
     modal.classList.remove("desactive")
     body.style.backgroundColor === 'rgba(0, 0, 0, 0.5)'
-    
+
 }
 
 const closeModal = (header, modal, body) => {
     modal.classList.add("desactive")
     modal.classList.remove("active")
-    
+
     setTimeout(() => {
         modal.style.display = 'none'
         header.style.display = "flex"
@@ -35,14 +35,49 @@ document.addEventListener('click', function (event) {
 
 const changeAlignItens = () => {
     const itens = document.querySelector('.header-links-content-mobile');
+    const gridItem = document.querySelectorAll('.grid-item')
     const largura = window.innerWidth;
 
-    if (largura <= 450) { 
+    if( largura <= 650){
+        for (let i = 0; i<gridItem.length; i++){
+            gridItem[i].classList.remove('grid-item-pc')
+            gridItem[i].classList.add('grid-item-mobile')
+        }
+    }
+
+    else{
+        for (let i = 0; i<gridItem.length; i++){
+            gridItem[i].classList.add('grid-item-pc')
+            gridItem[i].classList.remove('grid-item-mobile')
+        }
+    }
+
+    if (largura <= 450) {
         itens.classList.add('padding-itens');
     }
-    else{
+    else {
         itens.classList.remove('padding-itens');
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const colors = ['#E84135','#FC490B', '#512BD3', '#FFDF00', '#FF9100', '#2D84CA']
+
+
+    const items = document.querySelectorAll('.grid-item');
+    const largura = window.innerWidth;
+
+    if (largura > 650) {
+        for (let i = 0; i < items.length; i++) {
+            items[i].addEventListener('mouseover', function () {
+                items[i].style.borderColor = colors[i]
+            });
+            items[i].addEventListener('mouseout', function () {
+                items[i].style.borderColor = '#282828';
+            });
+        }
+    }
+
+});
 
 window.addEventListener('resize', changeAlignItens);
