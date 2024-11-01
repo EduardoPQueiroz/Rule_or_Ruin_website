36e35f03fb5359cae522e7c5a27e51bd082c5daa
Matheus Pinter <matheuspinter08@gmail.com>
2024-11-01 12:33:54 -0300

    fix: Fixed responsivity

--- js/script.js
+++ js/script.js
@@ -51,29 +51,20 @@ const changeAlignItens = () => {
     const largura = window.innerWidth;
 
     gridItems.forEach(item => {
-        item.classList.toggle('grid-item-pc', largura > 650);
+        // item.classList.toggle('grid-item-pc', largura > 650);
         item.classList.toggle('grid-item-mobile', largura <= 650);
     });
 
     itens.classList.toggle('padding-itens', largura <= 450);
-}
 
-const setupHoverEffects = () => {
-    const colors = ['#E84135', '#FC490B', '#512BD3', '#FFDF00', '#FF9100', '#2D84CA'];
-    const items = document.querySelectorAll('.grid-item');
-
-    items.forEach((item, index) => {
-        item.addEventListener('mouseover', () => {
-            item.style.borderColor = colors[index];
-        });
-        item.addEventListener('mouseout', () => {
-            item.style.borderColor = '#282828';
-        });
-    });
+    if(largura<650 && window.innerHeight<400){
+        gridItems.forEach(item =>{
+            item.style.fontSize = '2px'
+        })
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    setupHoverEffects();
     changeAlignItens();
 });
 
