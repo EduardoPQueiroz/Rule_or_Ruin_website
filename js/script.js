6a157b46c73b2c4e8afe2b5caa64b62bc2385b17
Matheus Pinter <matheuspinter08@gmail.com>
2024-11-04 19:07:14 -0300

    feat: Added team image

--- js/script.js
+++ js/script.js
@@ -107,9 +107,10 @@ function aboutSubjective(element) {
         const index = GETElement(element);
         titulo.innerHTML = titulos[index - 1]
         texto.innerHTML = conteudo[index - 1]
-        top.classList.remove('top-none');
-        top.classList.remove('desactiveSubjective')
+
+        top.classList.remove('desactiveSubjective', 'top-none')
         top.classList.add('activeSubjective')
+        // top.classList.remove('top-none');
         setTimeout(() => {
             activateOverlay(elements);
         }, 400)
